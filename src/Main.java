4e37c62eca1bb61dcd2a25df383f6fe237a8b0cd
SoloMaksimilian <mephistosmad@gmail.com>
2024-04-15 19:21:16 +0300

    added friends

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,9 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
-
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
 
         dog = dog - 3.5;
         cat = cat - 1.6;
@@ -25,6 +17,14 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
+        var friends = 19;
+        System.out.println("frinds=" + friends);
+
+        friends = friends + 2;
+        System.out.println("frinds+2=" + friends);
+
+        friends = friends / 7;
+        System.out.println("frinds/7=" + friends);
 
 
     }
